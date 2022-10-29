d46a7bf453ca9fdf0e2169a259f29e27a82cb271
Rubens Mendonça <rubens2005@gmail.com>
2022-10-29 13:34:28 -0300

    Update dag_executa_sh.py

--- dag_executa_sh.py
+++ dag_executa_sh.py
@@ -2,19 +2,27 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
-from airflow.contrib.hooks import SSHHook
+from airflow.contrib.hooks.ssh_hook import SSHHook
 
-sshHook = SSHHook(conn_id=SSH_ETL_DESENV)
+def executa_sh():
+    ssh = SSHHook(ssh_conn_id=SSH_ETL_DESENV)
+    ssh_client = None
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        ssh_client.exec_command('/home/etl/etl/sh/teste_rubens.sh -h')
+    finally:
+        if ssh_client:
+            ssh_client.close()
 
 with DAG(
     "dag_executa_sh",start_date = datetime(2022,10,5),
     schedule_interval = '5 * * * *', catchup = False) as dag:
     
-    executa_sh = SSHExecuteOperator(
-        task_id="executa_sh",
-        bash_command="/home/etl/etl/sh/teste_rubens.sh -h",
-        ssh_hook=sshHook,
+    call_ssh_task = PythonOperator(
+        task_id='call_ssh_task',
+        python_callable=executa_sh,
         dag=dag
     )
 
-    [executa_sh]
+    [call_ssh_task]
