5173f3b56a807756e1d20bd14ab364854ec1f88e
Rubens Mendonça <rubens2005@gmail.com>
2022-11-01 10:16:00 -0300

    Update KettleGetBeerTags.py

--- KettleGetBeerTags.py
+++ KettleGetBeerTags.py
@@ -1,22 +1,28 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.contrib.hooks.ssh_hook import SSHHook
 
+def executa_sh():
+    ssh = SSHHook(ssh_conn_id='SSH_ETL_DESENV')
+    ssh_client = None
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        ssh_client.exec_command('/home/etl/data-integration9.3/kitchen.sh -file=/home/etl/etl/tmp/COPIA_ARQUIVO.ktr')
+    finally:
+        if ssh_client:
+            ssh_client.close()
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2022, 10, 1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+with DAG(
+    "KettleGetBeerTags",start_date = datetime(2022,11,1),
+    schedule_interval = '5 * * * *', catchup = False) as dag:
+    
+    call_ssh_task = PythonOperator(
+        task_id='call_ssh_task',
+        python_callable=executa_sh,
+        dag=dag
+    )
 
-dag = DAG('KettleGetBeerTags', default_args=default_args, schedule_interval=timedelta(days=1))
-
-t1 = BashOperator(
-    task_id='RunKettle',
-    bash_command='/Users/codek/apps/kettle-remix-20190130/data-integration/kitchen.sh -file=/Users/codek/projects/airflow/jb-extract-beer-tags.kjb',
-    dag=dag)
+    [call_ssh_task]
