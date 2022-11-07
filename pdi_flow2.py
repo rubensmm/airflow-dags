e03350aed7a03d518ecea5c682266ee10ebd6fce
Rubens Mendonça <rubens2005@gmail.com>
2022-11-07 14:53:36 -0300

    Create pdi_flow2.py

---
+++ pdi_flow2.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from airflow_pentaho.operators.kettle import KitchenOperator
+from airflow_pentaho.operators.kettle import PanOperator
+from airflow_pentaho.operators.carte import CarteJobOperator
+from airflow_pentaho.operators.carte import CarteTransOperator
+
+DAG_NAME = 'pdi_flow2'
+DEFAULT_ARGS = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'email': ['airflow@example.com'],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'email_on_failure': False,
+    'email_on_retry': False
+}
+
+with DAG(dag_id=DAG_NAME,
+         default_args=DEFAULT_ARGS,
+         dagrun_timeout=timedelta(hours=2),
+         schedule_interval='30 0 * * *') as dag:
+
+      trans1 = CarteTransOperator(
+        dag=dag,
+        task_id='trans1',
+        trans='C:\tmp\COPIA_ARQUIVO',
+        params={'date': '{{ ds }}'})
+  
+      [trans1]
