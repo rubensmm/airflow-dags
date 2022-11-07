6b553a10eb3d7b999bed8f2473f1e32f62ae1c60
Rubens Mendonça <rubens2005@gmail.com>
2022-11-07 14:02:03 -0300

    Update pdi_flow.py

--- pdi_flow.py
+++ pdi_flow.py
@@ -31,4 +31,11 @@ with DAG(dag_id=DAG_NAME,
         trans='C:/tmp/COPIA_ARQUIVO',
         params={'date': '{{ ds }}'})
         
-      [trans1]
+      enriche_customers = CarteTransOperator(
+        conn_id='pdi_default',
+        task_id="enrich_customer_data",
+        job="C:/tmp/COPIA_ARQUIVO",
+        params={"date": "{{ ds }}"},  # Date in yyyy-mm-dd format
+        dag=dag)
+  
+      [enriche_customers]
