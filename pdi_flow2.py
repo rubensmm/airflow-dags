6693a5aa6442a87555ef6a10ac3341b5a2546b4c
Rubens Mendonça <rubens2005@gmail.com>
2022-11-10 10:04:57 -0300

    .

--- pdi_flow2.py
+++ pdi_flow2.py
@@ -28,14 +28,16 @@ with DAG(dag_id=DAG_NAME,
       loop = CarteTransOperator(
         dag=dag,
         task_id='loop',
-        trans='/home/rmendonc/data-integration/transformacoes/LOOP.ktr',
+        #trans='/home/rmendonc/data-integration/transformacoes/LOOP.ktr',
+        trans='C:/tmp/LOOP.ktr',
         params={'date': '{{ ds }}'}
       )
 
       copia = CarteTransOperator(
         dag=dag,
         task_id='copia',
-        trans='/home/rmendonc/data-integration/transformacoes/COPIA_ARQUIVO.ktr',
+        #trans='/home/rmendonc/data-integration/transformacoes/COPIA_ARQUIVO.ktr',
+        trans='C:/tmp/COPIA_ARQUIVO.ktr',
         params={'date': '{{ ds }}'}
       )
 
