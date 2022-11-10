b76d0dcd24713cfa1f3b26abbe87341b8eff3edf
Rubens Mendonça <rubens2005@gmail.com>
2022-11-10 16:29:33 -0300

    .

--- pdi_flow.py
+++ pdi_flow.py
@@ -28,7 +28,7 @@ with DAG(dag_id=DAG_NAME,
       copia = CarteTransOperator(
         dag=dag,
         task_id='copia',
-        trans='C:/tmp/ETL_AIRFLOW/COPIA_ARQUIVO.ktr',
+        trans='C:/tmp/ETL_AIRFLOW/COPIA_ARQUIVO',
         params={'date': '{{ ds }}'}
       )
   
