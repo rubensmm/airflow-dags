918152fd0f1cca4d6b81f6275aa06cc4b211e69a
Rubens Mendonça <rubens2005@gmail.com>
2022-11-14 12:55:45 -0300

    .

--- pdi_flow2.py
+++ pdi_flow2.py
@@ -23,7 +23,7 @@ DEFAULT_ARGS = {
 with DAG(dag_id=DAG_NAME,
          default_args=DEFAULT_ARGS,
          dagrun_timeout=timedelta(hours=2),
-         schedule_interval='30 0 * * *') as dag:
+         schedule_interval='30 0 * * *', catchup = False) as dag:
 
       loop = CarteTransOperator(
         dag=dag,
