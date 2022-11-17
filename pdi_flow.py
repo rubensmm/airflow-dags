bb021fac42eaa061734ddc6c723117d4a10d9e72
Rubens Mendonça <rubens2005@gmail.com>
2022-11-17 09:41:34 -0300

    Update pdi_flow.py

--- pdi_flow.py
+++ pdi_flow.py
@@ -29,7 +29,7 @@ with DAG(dag_id=DAG_NAME,
         dag=dag,
         pdi_conn_id='pdi_default',
         task_id='copia',
-        trans='LOOP',
+        trans='LOOP2',
         params={'date': '{{ ds }}'}
       )
   
